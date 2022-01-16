2ffe8d6229707169e7ff76d9f17bf871e14866e3
kariswanjiru <wanjiruestar@gmail.com>
2022-01-16 12:21:23 +0300

    Testing without Home component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 //components 
 import Header from './components/Header';
-import Home from './components/Home'
+//import Home from './components/Home'
+//<Home/>
 //Styles
 import { GlobalStyle } from './GlobalStyle';
 
@@ -11,13 +12,15 @@ function App() {
   return (
     <div className='App'>
       <Header/>
-      <Home/>
+     
       <GlobalStyle/>
     </div>
   )
 }
 
 export default App;
+
+//this was a trail lamp code
 //SWitching a lamp on
 
 //Components
